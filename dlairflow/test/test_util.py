5a30fcd0847f18f2f1c9cbfed1ccdb26a64669d2
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2025-01-23 11:18:17 -0700

    add ensure_sql

--- dlairflow/test/test_util.py
+++ dlairflow/test/test_util.py
@@ -3,10 +3,18 @@
 """Test dlairflow.util.
 """
 import os
-from ..util import user_scratch
+from ..util import user_scratch, ensure_sql
+from .test_postgresql import temporary_airflow_home
 
 
 def test_user_scratch():
     """Test scratch dir.
     """
     assert user_scratch() == os.path.join('/data0', 'datalab', os.environ['USER'])
+
+
+def test_ensure_sql(temporary_airflow_home):
+    """Test SQL directory creation.
+    """
+    assert ensure_sql() == str(temporary_airflow_home / 'dags' / 'sql')
+    assert os.path.isdir(str(temporary_airflow_home / 'dags' / 'sql'))
