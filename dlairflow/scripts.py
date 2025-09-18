27bf030defb6794939505b6c93859ea9b68c7899
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2025-09-18 14:28:30 -0700

    add utility script.

---
+++ dlairflow/scripts.py
@@ -0,0 +1,26 @@
+# Licensed under a BSD-style 3-clause license - see LICENSE.md.
+# -*- coding: utf-8 -*-
+"""
+dlairflow.scripts
+=================
+
+Entry points for command-line scripts.
+"""
+import os
+import glob
+from .util import ensure_sql
+
+
+def clean_sql_templates():
+    """Entry-point for :command:`clean_sql_templates`.
+
+    Returns
+    -------
+    :class:`int`
+        An integer suitable for passing to :func:`sys.exit`.
+    """
+    sql_dir = ensure_sql()
+    template_files = glob.glob(os.path.join(sql_dir, 'dlairflow.postgresql.*.sql'))
+    for tf in template_files:
+        os.remove(tf)
+    return 0
---
+++ dlairflow/test/test_scripts.py
@@ -0,0 +1,25 @@
+# Licensed under a BSD-style 3-clause license - see LICENSE.md.
+# -*- coding: utf-8 -*-
+"""Test dlairflow.scripts.
+"""
+import os
+import pytest
+from ..scripts import clean_sql_templates
+from .test_postgresql import temporary_airflow_home
+
+
+def test_clean_sql_templates(temporary_airflow_home):
+    """Test clean_sql_templates.
+    """
+    function_names = ('one', 'two', 'three')
+    for function_name in function_names:
+        full_name = str(temporary_airflow_home / 'dags' / 'sql' /
+                        f'dlairflow.postgresql.{function_name}.sql')
+        with open(full_name, 'w') as SQL:
+            SQL.write(f'--\n-- {function_name}\n--\n')
+        assert os.path.exists(full_name)
+    assert clean_sql_templates() == 0
+    for function_name in function_names:
+        full_name = str(temporary_airflow_home / 'dags' / 'sql' /
+                        f'dlairflow.postgresql.{function_name}.sql')
+        assert not os.path.exists(full_name)
