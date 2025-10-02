083644690b763a00ca08b40e7ad178044ff7e900
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2025-10-02 14:14:00 -0700

    add options to clean-up script

--- dlairflow/test/test_scripts.py
+++ dlairflow/test/test_scripts.py
@@ -3,12 +3,13 @@
 """Test dlairflow.scripts.
 """
 import os
+import sys
 import pytest
 from ..scripts import clean_dlairflow_sql_templates
 from .test_postgresql import temporary_airflow_home
 
 
-def test_clean_sql_templates(temporary_airflow_home):
+def test_clean_sql_templates(temporary_airflow_home, monkeypatch):
     """Test clean_sql_templates.
     """
     sql_dir = str(temporary_airflow_home / 'dags' / 'sql')
@@ -19,7 +20,9 @@ def test_clean_sql_templates(temporary_airflow_home):
         with open(full_name, 'w') as SQL:
             SQL.write(f'--\n-- {function_name}\n--\n')
         assert os.path.exists(full_name)
-    assert clean_dlairflow_sql_templates() == 0
-    for function_name in function_names:
-        full_name = os.path.join(sql_dir, f'dlairflow.postgresql.{function_name}.sql')
-        assert not os.path.exists(full_name)
+    with monkeypatch.context() as m:
+        m.setattr(sys, 'argv', ['clean_dlairflow_sql_templates'])
+        assert clean_dlairflow_sql_templates() == 0
+        for function_name in function_names:
+            full_name = os.path.join(sql_dir, f'dlairflow.postgresql.{function_name}.sql')
+            assert not os.path.exists(full_name)
