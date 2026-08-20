08c6632543fd22d8fff7652710322d7ff6938e2d
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2026-08-20 15:49:00 -0700

    fix style

--- dlairflow/test/test_load.py
+++ dlairflow/test/test_load.py
@@ -32,6 +32,7 @@ def test_load_table(temporary_airflow_home, task_function, connection, schema, t
     else:
         assert test_operator.env['PGHOST'] == '{{ conn.get(params.connection).host }}'
     if schema is None:
-        assert test_operator.env['FITS2DB_FILE'] == '{{ params.load_dir }}/{{ params.schema }}.{{ params.table }}.fits'
+        assert test_operator.env['FITS2DB_FILE'] == ('{{ params.load_dir }}/' +
+                                                     '{{ params.schema }}.{{ params.table }}.fits')
     else:
         assert test_operator.env['FITS2DB_FILE'] == 'load_dir/schema.table.fits'
